cf062392466160e1fb83d06c082209a283acc588
Robert Bragg <robert@linux.intel.com>
2012-08-15 20:14:04 +0100

    Adds tutorial templating support

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # template.html file can have any number of "mustache" markers like {{foo}} and
 # {{bar}}
@@ -26,3 +26,27 @@ do
     sed -i 's/{{\([a-zA-Z_-]\+\)}}//' www/`basename $page`.html
 done
 
+if test "$COGL_SRC" = ""; then
+  COGL_SRC=.
+fi
+
+for tutorial in `find $COGL_SRC/tutorials -maxdepth 1 -mindepth 1 -iname '*.c'`
+do
+  tutorial=`basename $tutorial`
+  tutorial=${tutorial%.c}
+  mkdir -p www/tutorials/
+
+  echo "Generating www/tutorials/$tutorial.html"
+
+  for marker in `grep '{{' tutorials/$tutorial.html|sed -e 's/{{\([a-zA-Z_-]\+\)}}/\1/'`
+  do
+      echo $marker
+      echo -n "  substituting {{$marker}} with $page/$marker.html: "
+      if csplit -q tutorials/$tutorial.c "%//BEGIN($marker)%1" "/\/\/END($marker)"; then
+        echo "OK"
+      else
+        echo "FAILED"
+      fi
+      sed -i "/{{$marker}}/ r xx00" www/tutorials/$tutorial.html
+  done
+done
